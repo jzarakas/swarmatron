b6ee2879aa79d4f5440a94dea95c9f1bbffa2562
Adam <adsalberg@gmail.com>
2015-01-21 18:46:26 -0500

    Switched AKInstrument from AKFMOscillator to AKVCOscillator.

---
+++ swarmatron/VCOsc.h
@@ -0,0 +1,13 @@
+//
+//  VCOsc.h
+//  swarmatron
+//
+//  Created by Adam Salberg on 1/21/15.
+//  Copyright (c) 2015 Adam Salberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VCOsc : UIViewController
+
+@end
---
+++ swarmatron/VCOsc.m
@@ -0,0 +1,37 @@
+//
+//  VCOsc.m
+//  swarmatron
+//
+//  Created by Adam Salberg on 1/21/15.
+//  Copyright (c) 2015 Adam Salberg. All rights reserved.
+//
+
+#import "VCOsc.h"
+
+@interface VCOsc ()
+
+@end
+
+@implementation VCOsc
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
